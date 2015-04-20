b927ca514c3e9cdd91bba65243c0909914ec916e
G. Ghez <gregory.ghez@thomsonreuters.com>
2015-04-20 18:49:24 +0200

    First commit

---
+++ src/index.coffee
@@ -0,0 +1,34 @@
+fs = require 'fs'
+exec = require 'child_process'
+  .exec
+
+getopt = require 'node-getopt'
+  .create [
+      ['d', 'target=ARG', 'Path to file or directory to watch']
+      ['r', 'retention=ARG', 'Retention (in ms.) before command (default: 1000ms)']
+      ['c', 'command=ARG', 'Command to execute after retention']
+      ['', 'help', 'Display this help']
+    ]
+  .bindHelp()
+
+opt = getopt.parseSystem()
+timer = null
+
+install = (cmd) ->
+  console.log "Running #{cmd}..."
+  proc = exec cmd, (stderr, stdout) ->
+    console.error stderr if stderr
+    console.log stdout if stdout
+    console.log 'Done.'
+
+if (target = opt.options['target'] || '.')
+  wOpts =
+    persistent: true
+    recursive: true
+  console.log 'Watching', target
+  fs.watch target, wOpts, (event, filename) ->
+  	console.log 'Watcher', event, filename
+  	clearTimeout timer if timer
+  	timer = setTimeout (-> install opt.options['command']), opt.options['retention'] || 1000
+else
+  getopt.showHelp()
---
+++ src/index.js
@@ -0,0 +1,44 @@
+var exec, fs, getopt, install, opt, target, timer, wOpts;
+
+fs = require('fs');
+
+exec = require('child_process').exec;
+
+getopt = require('node-getopt').create([['d', 'target=ARG', 'Path to file or directory to watch'], ['r', 'retention=ARG', 'Retention (in ms.) before command (default: 1000ms)'], ['c', 'command=ARG', 'Command to execute after retention'], ['', 'help', 'Display this help']]).bindHelp();
+
+opt = getopt.parseSystem();
+
+timer = null;
+
+install = function(cmd) {
+  var proc;
+  console.log("Running " + cmd + "...");
+  return proc = exec(cmd, function(stderr, stdout) {
+    if (stderr) {
+      console.error(stderr);
+    }
+    if (stdout) {
+      console.log(stdout);
+    }
+    return console.log('Done.');
+  });
+};
+
+if ((target = opt.options['target'] || '.')) {
+  wOpts = {
+    persistent: true,
+    recursive: true
+  };
+  console.log('Watching', target);
+  fs.watch(target, wOpts, function(event, filename) {
+    console.log('Watcher', event, filename);
+    if (timer) {
+      clearTimeout(timer);
+    }
+    return timer = setTimeout((function() {
+      return install(opt.options['command']);
+    }), opt.options['retention'] || 1000);
+  });
+} else {
+  getopt.showHelp();
+}
